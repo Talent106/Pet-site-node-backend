161807863d879d6088074f5144e1755db55f0623
Kimura Yuu <joaovictor9910@outlook.com>
2024-09-18 18:09:10 -0300

    fix: 404 error in app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1/auth', indexRouter);
-app.use('/product', productsRouter);
+app.use('/v1/product', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
