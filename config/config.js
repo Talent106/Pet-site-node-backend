aa825ebe046efc04b05113206187c9f48d4dcdcd
Kimura Yuu <joaovictor9910@outlook.com>
2024-09-18 00:57:55 -0300

    fix: connected mongooDB

--- config/config.js
+++ config/config.js
@@ -1,5 +1,18 @@
-module.exports = {
-    port: process.env.PORT || 3000,
-    mongodb_uri: process.env.MONGODB_URI || 'mongodb://localhost/mydb',
-    // Add other configuration options here
+var mongoose = require('mongoose');
+
+const server_url = 'mongodb://127.0.0.1:27017/pet_db';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(server_url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(' mongodb connected... ');
+    } catch (err) {
+        console.error('MongoDB connection err', err.message);
+        process.exit(1)
+    }
 };
+
+module.exports = { connectDB };
